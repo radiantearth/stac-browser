71355e5039099815a77a1b3c72fdc90066e0b645
Seth Fitzsimmons <seth@mojodna.net>
2019-07-17 13:33:13 -0700

    Fall back to displaying the license ID if it doesn't resolve

--- src/components/common.js
+++ src/components/common.js
@@ -156,7 +156,7 @@ export default {
         return this._license;
       }
 
-      return spdxToHTML(this._license);
+      return spdxToHTML(this._license) || this._license;
     },
     licenseUrl() {
       if (this._license === "proprietary") {
