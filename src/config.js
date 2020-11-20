fe1f6a95c63775a203be4f94c8e3f503074b384c
Sam Sipe <samsipe@gmail.com>
2020-11-20 12:11:04 -0500

    Update packages, link s2-l2a-cogs

--- src/config.js
+++ src/config.js
@@ -1,17 +1,13 @@
 export const CATALOG_URL =
   process.env.CATALOG_URL ||
-  "https://raw.githubusercontent.com/cholmes/sample-stac/master/stac/catalog.json";
+  "https://earth-search.aws.element84.com/v0/collections/sentinel-s2-l2a-cogs";
 
-export const STAC_VERSION =
-  process.env.STAC_VERSION ||
-  "0.9.0";
+export const STAC_VERSION = process.env.STAC_VERSION || "1.0.0-beta.2";
 
 export const TILE_SOURCE_TEMPLATE =
   process.env.TILE_SOURCE_TEMPLATE ||
   "https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url={ASSET_HREF}";
 
-export const STAC_PROXY_URL =
-  process.env.STAC_PROXY_URL;
+export const STAC_PROXY_URL = process.env.STAC_PROXY_URL;
 
-export const TILE_PROXY_URL =
-  process.env.TILE_PROXY_URL
+export const TILE_PROXY_URL = process.env.TILE_PROXY_URL;
