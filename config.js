ff1ad3182cccec50604dabca5b7c3b1348687a19
Jay <jlaura@asu.edu>
2021-08-13 06:46:47 -0700

    hacked in wms example - first cut

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    catalogUrl: "https://raw.githubusercontent.com/cholmes/pdd-stac/beta2/disasters/collection.json",
-    catalogTitle: "STAC Browser",
+    catalogUrl: "http://asc-moon.s3-us-west-2.amazonaws.com/kaguyatc_dtms/collection.json",
+    catalogTitle: "dev STAC browser",
     tileSourceTemplate: "https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url={ASSET_HREF}",
     stacProxyUrl: null,
     tileProxyUrl: null,
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,7 +2,15 @@
   <section class="mb-4">
     <l-map class="map" :class="stac.type" ref="leaflet" @ready="init()">
       <l-control-fullscreen />
-      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" :options="mapOptions" />
+      <l-wms-tile-layer
+          :key="getBaseMap.name"
+          :base-url="getBaseMap.baseUrl"
+          :crs="getBaseMap.crs"
+          :name="getBaseMap.name"
+          :attribution="getBaseMap.attribution"
+          :layers="getBaseMap.name"
+          :format="getBaseMap.format"
+          layer-type="base"/>
       <!-- ToDo: Replace with STAC Leaflet plugin; use minimap plugin? -->
       <l-geo-json v-if="isGeoJSON" ref="bounds" @ready="fitBounds" :geojson="stac" />
       <l-rectangle v-else-if="bbox" ref="bounds" @ready="fitBounds" :bounds="bbox" />
@@ -12,7 +20,8 @@
 
 <script>
 // import L from 'leaflet';
-import { LMap, LGeoJson, LRectangle, LTileLayer } from 'vue2-leaflet';
+import { CRS } from "leaflet";
+import { LMap, LGeoJson, LRectangle, LWMSTileLayer } from 'vue2-leaflet';
 import LControlFullscreen from 'vue2-leaflet-fullscreen';
 import 'leaflet/dist/leaflet.css';
 
@@ -23,15 +32,12 @@ export default {
     LGeoJson,
     LMap,
     LRectangle,
-    LTileLayer
+    'l-wms-tile-layer': LWMSTileLayer
   },
   data() {
     return {
       map: null,
-      boundsLayer: null,
-      mapOptions: {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors.'
-      }
+      boundsLayer: null
     };
   },
   props: {
@@ -50,7 +56,36 @@ export default {
         return [[bbox[1], bbox[2]], [bbox[3], bbox[0]]];
       }
       return null;
-    }
+    },
+    getBaseMap() {
+      if ('summaries' in this.stac) {
+        if ('ssys:targets' in this.stac.summaries){
+          var target = this.stac.summaries['ssys:targets'][0]
+          switch (target.normalize()) {
+            case "moon":
+                return {"name":"LROC_WAC",
+                    "baseUrl":"https://planetarymaps.usgs.gov/cgi-bin/mapserv?map=/maps/earth/moon_simp_cyl.map",
+                    "crs":CRS.EPSG4326,
+                    "attribution":"USGS Astrogeology",
+                    "format":"image/png"
+                    }
+          }
+
+        }
+      }
+      // Our items still need the ssys:targets implemented, so this just hard codes to the moon for now. Will
+      // check for ssys:targets in properties at some point in the future.
+      if ('properties' in this.stac) {
+          return {"name":"LROC_WAC",
+                  "baseUrl":"https://planetarymaps.usgs.gov/cgi-bin/mapserv?map=/maps/earth/moon_simp_cyl.map",
+                  "crs":CRS.EPSG4326,
+                  "attribution":"USGS Astrogeology",
+                  "format":"image/png"
+                  }
+        
+      }
+    return {}
+    }        
   },
   methods: {
     init() {
@@ -63,3 +98,4 @@ export default {
   }
 }
 </script>
+
