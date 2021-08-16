a4cc933826d683b7e29b848cdfadbac51d431ea2
jlaura <jlaura@usgs.gov>
2021-08-16 11:20:32 -0700

    hacked in wms example - first cut (#88)
    * hacked in wms example - first cut
    
    * Update Map.vue
    
    * Code review
    
    Co-authored-by: Matthias Mohr <m.mohr@uni-muenster.de>

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,20 +1,24 @@
 <template>
   <section class="mb-4">
     <l-map class="map" :class="stac.type" ref="leaflet" @ready="init()">
-      <l-control-fullscreen />
-      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" :options="mapOptions" />
+      <LControlFullscreen />
+      <template v-if="baseMaps.length > 0">
+        <component :is="baseMap.component" v-for="baseMap in baseMaps" :key="baseMap.name" v-bind="baseMap" :layers="baseMap.name" layer-type="base" />
+      </template>
+      <LTileLayer v-else url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" :options="mapOptions" />
       <!-- ToDo: Replace with STAC Leaflet plugin; use minimap plugin? -->
-      <l-geo-json v-if="isGeoJSON" ref="bounds" @ready="fitBounds" :geojson="stac" />
-      <l-rectangle v-else-if="bbox" ref="bounds" @ready="fitBounds" :bounds="bbox" />
+      <LGeoJson v-if="isGeoJSON" ref="bounds" @ready="fitBounds" :geojson="stac" />
+      <LRectangle v-else-if="bbox" ref="bounds" @ready="fitBounds" :bounds="bbox" />
     </l-map>
   </section>
 </template>
 
 <script>
-// import L from 'leaflet';
-import { LMap, LGeoJson, LRectangle, LTileLayer } from 'vue2-leaflet';
+import { CRS } from "leaflet";
+import { LMap, LGeoJson, LRectangle, LTileLayer, LWMSTileLayer } from 'vue2-leaflet';
 import LControlFullscreen from 'vue2-leaflet-fullscreen';
 import 'leaflet/dist/leaflet.css';
+import Utils from '../utils';
 
 export default {
   name: 'Map',
@@ -23,7 +27,8 @@ export default {
     LGeoJson,
     LMap,
     LRectangle,
-    LTileLayer
+    LTileLayer,
+    LWMSTileLayer
   },
   data() {
     return {
@@ -50,7 +55,46 @@ export default {
         return [[bbox[1], bbox[2]], [bbox[3], bbox[0]]];
       }
       return null;
-    }
+    },
+    baseMaps() {
+      let targets = [];
+      if (this.stac.isCollection() && Utils.isObject(this.stac.summaries) && Array.isArray(this.stac.summaries['ssys:targets'])) {
+        targets = this.stac.summaries['ssys:targets'];
+      }
+      else if (this.stac.isCollection() && Array.isArray(this.stac['ssys:targets'])) {
+        targets = this.stac['ssys:targets'];
+      }
+      else if (this.stac.isItem() && Array.isArray(this.stac.properties['ssys:targets'])) {
+        targets = this.stac.properties['ssys:targets'];
+      }
+
+      const baseMaps = {
+        europa: {
+          baseUrl: "https://planetarymaps.usgs.gov/cgi-bin/mapserv?map=/maps/jupiter/europa_simp_cyl.map",
+          name: "GALILEO_VOYAGER"
+        },
+        mars: {
+          baseUrl: "https://planetarymaps.usgs.gov/cgi-bin/mapserv?map=/maps/mars/mars_simp_cyl.map",
+          name: "MDIM21"
+        },
+        moon: {
+          baseUrl: "https://planetarymaps.usgs.gov/cgi-bin/mapserv?map=/maps/earth/moon_simp_cyl.map",
+          name: "LROC_WAC"
+        }
+      };
+
+      return targets.map(target => {
+        target = target.toLowerCase();
+        if (baseMaps[target]) {
+          return Object.assign({
+              component: "LWMSTileLayer",
+              crs: CRS.EPSG4326,
+              attribution: "USGS Astrogeology",
+              format: "image/png"
+            }, baseMaps[target]);
+        }
+      });
+    }        
   },
   methods: {
     init() {
