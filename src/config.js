a51e464f40d8fabb38aa296f81d01980942d7aad
Ian Shiland <ishiland@gmail.com>
2021-11-19 09:07:20 -0500

    relative path for catalog

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_CATALOG_URL = process.env.CATALOG_URL;
+export const BASE_CATALOG_URL =  window.location.href + "/catalog.json"
 export const TILE_SOURCE_TEMPLATE = process.env.TILE_SOURCE_TEMPLATE;
 export const STAC_PROXY_URL = process.env.STAC_PROXY_URL;
 export const TILE_PROXY_URL = process.env.TILE_PROXY_URL;
