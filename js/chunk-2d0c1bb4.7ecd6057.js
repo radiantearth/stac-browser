7c1bc04086bf36a50e5fd1f45b37b2eba6bf8640
STAC Browser CI <matthias.mohr@radiant.earth>
2021-12-14 19:14:27 +0000

    deploy: aa0ca6ee231ddb163a6794d7da9ac4e19a05e15d

--- js/chunk-2d0c1bb4.7ecd6057.js
+++ js/chunk-2d0c1bb4.7ecd6057.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c1bb4"],{"46fb":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("b-alert",{attrs:{variant:"warning",show:""}},[n("strong",[e._v("Deprecated")]),e._v("  "),n("small",[e._v("Please note that this "+e._s(e.type)+" is deprecated with the potential to be removed in any of the next versions. It should be transitioned out of usage as soon as possible. Refrain from using it in new projects.")])])},o=[],a={name:"DeprecationNotice",props:{type:String}},i=a,r=n("2877"),l=Object(r["a"])(i,s,o,!1,null,null,null);t["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d0c1bb4.4269dfbe.js.map
+//# sourceMappingURL=chunk-2d0c1bb4.7ecd6057.js.map
