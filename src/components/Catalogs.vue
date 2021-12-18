d9b766da679c1996e883c2452b6831ad6b24cb0e
Matthias Mohr <m.mohr@uni-muenster.de>
2021-12-18 20:05:42 +0100

    First experiments with sorting

--- src/components/Catalogs.vue
+++ src/components/Catalogs.vue
@@ -3,9 +3,10 @@
     <h2>
       Catalogs
       <template v-if="!hasMore">({{ catalogs.length }})</template>
+      <SortButtons class="ml-4" v-model="sort" />
     </h2>
     <b-card-group columns>
-      <Catalog v-for="catalog in catalogs" :catalog="catalog" :key="catalog.href" />
+      <Catalog v-for="catalog in sortedCatalogs" :catalog="catalog" :key="catalog.href" />
     </b-card-group>
     <b-button v-if="hasMore" @click="loadMore" variant="primary" v-b-visible.200="loadMore">Load more...</b-button>
   </section>
@@ -17,7 +18,8 @@ import Catalog from './Catalog.vue';
 export default {
   name: "Catalogs",
   components: {
-    Catalog
+    Catalog,
+    SortButtons: () => import('./SortButtons.vue')
   },
   props: {
     catalogs: {
@@ -27,6 +29,25 @@ export default {
     hasMore: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      sort: 0
+    };
+  },
+  computed: {
+    sortedCatalogs() {
+      let catalogs = this.catalogs;
+      if (this.sort !== 0) {
+        catalogs = catalogs.slice(0).sort((a,b) => {
+          return (a.title || a.id || "").localeCompare((b.title || b.id || ""));
+        });
+        if (this.sort === -1) {
+          catalogs = catalogs.reverse();
+        }
+      }
+      return catalogs;
     }
   },
   methods: {
---
+++ src/components/SortButtons.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-button-group title="Sort" size="sm">
+    <b-button @click="toggle(1)" :pressed="value === 1"><b-icon-sort-alpha-down /></b-button>
+    <b-button @click="toggle(-1)" :pressed="value === -1"><b-icon-sort-alpha-up /></b-button>
+  </b-button-group>
+</template>
+
+<script>
+import { BIconSortAlphaDown, BIconSortAlphaUp } from 'bootstrap-vue';
+
+export default {
+  id: "SortButtons",
+  components: {
+    BIconSortAlphaDown,
+    BIconSortAlphaUp
+  },
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    toggle(selectedValue) {
+      let newValue = 0;
+      if(this.value !== selectedValue) {
+        newValue = selectedValue;
+      }
+      this.$emit('input', newValue);
+    }
+  }
+}
+</script>
