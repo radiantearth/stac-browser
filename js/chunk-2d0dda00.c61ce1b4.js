3c3762e4cec8dfde53a88d4f366ffbe594e6d43b
STAC Browser CI <matthias.mohr@radiant.earth>
2022-04-11 21:25:31 +0000

    deploy: 0376bae0cb5118639275f519189e929e93f53cb9

---
+++ js/chunk-2d0dda00.c61ce1b4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0dda00"],{"81d5":function(e,r,s){"use strict";s.r(r);var t=function(){var e=this,r=e.$createElement,s=e._self._c||r;return s("p",{staticClass:"keywords d-flex flex-wrap"},e._l(e.keywords,(function(r){return s("b-badge",{key:r,staticClass:"mr-1 mb-1",attrs:{variant:"secondary"}},[e._v(e._s(r))])})),1)},a=[],n={name:"Keywords",props:{keywords:{type:Array,required:!0}}},d=n,l=s("2877"),o=Object(l["a"])(d,t,a,!1,null,null,null);r["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0dda00.c61ce1b4.js.map
