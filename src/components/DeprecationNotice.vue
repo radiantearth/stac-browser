f2b5cdd5afa51e264c8ea88cbb366be205253017
Matthias Mohr <m.mohr@uni-muenster.de>
2023-01-03 15:31:33 +0100

    More translations...

--- src/components/DeprecationNotice.vue
+++ src/components/DeprecationNotice.vue
@@ -2,20 +2,18 @@
   <b-alert :variant="variant" show>
     <strong>{{ title }}</strong>&nbsp;
     <small v-if="isDeprecated">
-      Please note that this {{ type }} is deprecated with the potential to be removed in any of the next versions.
-      It should be transitioned out of usage as soon as possible. Refrain from using it in new projects.
+      {{ $t('deprecation.warning', {type}) }}
     </small>
-    <small v-else>This {{ type }} is <em>not</em> deprecated, but there are other versions available:</small>
+    <small v-else>{{ $t('deprecation.otherVersionsNotice', {type}) }}</small>
     <ul v-if="latestLink || successorLink || predecessorLink">
-      <li v-if="latestLink"><small><StacLink :data="latestLink" fallbackTitle="Go to latest version" tooltip="Go to latest version" /></small></li>
-      <li v-if="successorLink"><small><StacLink :data="successorLink" fallbackTitle="Go to newer version" tooltip="Go to newer version" /></small></li>
-      <li v-if="predecessorLink"><small><StacLink :data="predecessorLink" fallbackTitle="Go to previous version" tooltip="Go to previous version" /></small></li>
+      <li v-if="latestLink"><small><StacLink :data="latestLink" :fallbackTitle="latestTitle" :tooltip="latestTitle" /></small></li>
+      <li v-if="successorLink"><small><StacLink :data="successorLink" :fallbackTitle="successorTitle" :tooltip="successorTitle" /></small></li>
+      <li v-if="predecessorLink"><small><StacLink :data="predecessorLink" :fallbackTitle="predecessorTitle" :tooltip="predecessorTitle" /></small></li>
     </ul>
   </b-alert>
 </template>
 
 <script>
-// TODO: I18N
 export default {
   name: 'DeprecationNotice',
   components: {
@@ -28,6 +26,15 @@ export default {
     }
   },
   computed: {
+    latestTitle() {
+      return this.$t('deprecation.goToLatest');
+    },
+    successorTitle() {
+      return this.$t('deprecation.goToSuccessor');
+    },
+    predecessorTitle() {
+      return this.$t('deprecation.goToPredecessor');
+    },
     latestLink() {
       return this.data.getStacLinkWithRel('latest-version');
     },
@@ -46,21 +53,27 @@ export default {
     },
     title() {
       if (this.isDeprecated) {
-        return 'Deprecated';
+        return this.$t('deprecated');
       }
       else if (this.latestLink || this.successorLink) {
-        return 'Outdated';
+        return this.$t('deprecation.outdatedTitle');
       }
       else {
-        return 'Other Versions';
+        return this.$t('deprecation.otherVersionsTitle');
       }
     },
     type() {
       if (this.data.isItem()) {
-        return "Item";
+        return this.$t('stacItem');
+      }
+      else if (this.data.isCollection()) {
+        return this.$t(`stacCollection`);
+      }
+      else if (this.data.isCatalog()) {
+        return this.$t(`stacCatalog`);
       }
       else {
-        return this.data.type;
+        return '';
       }
     }
   }
