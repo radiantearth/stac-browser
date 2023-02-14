5c1947c3b51d19868d552c7361b50f51a1dbd8f1
rnanclares <raulnanclares@gmail.com>
2023-02-14 16:18:16 -0600

    Fixed small typos

--- src/locales/es/default.js
+++ src/locales/es/default.js
@@ -1,6 +1,7 @@
 export default Object.assign(
-  // Don't import the fields as it's a 1:1 mapping anyway
-  // { fields: require('./fields.json') },
+  {
+    fields: require('./fields.json'),
+  },
   require('./texts.json'),
   require('./custom.json')
 );
