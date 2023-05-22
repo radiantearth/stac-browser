52598d623dbca5f896dffa70ef058c991a655bc6
silvester-pari <silvester.pari@eox.at>
2023-05-22 09:28:39 +0200

    chore: add path prefix for bundling

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
     displayGeoTiffByDefault: false,
     buildTileUrlTemplate: ({href, asset}) => "https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url=" + encodeURIComponent(asset.href.startsWith("/vsi") ? asset.href : href),
     stacProxyUrl: null,
-    pathPrefix: "/",
+    pathPrefix: "/stac-browser/",
     historyMode: "history",
     cardViewMode: "list",
     showThumbnailsAsAssets: false,
