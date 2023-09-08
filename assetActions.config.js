aa24c1d30e864682d76e5b4a2b5ac07575607c43
Matthias Mohr <webmaster@mamo-net.de>
2023-09-08 15:59:49 +0200

    Remove cogeo.xyz action #368

--- assetActions.config.js
+++ assetActions.config.js
@@ -1,11 +1,9 @@
 import CopcViewer from './src/actions/assets/CopcViewer.js';
-//import CoGeoXyz from './src/actions/assets/CoGeoXyz.js';
 import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
 //import Felt from './src/actions/assets/Felt.js';
 
 export default {
   CopcViewer,
-//CoGeoXyz,
   GeoJsonIo,
 //Felt
 };
