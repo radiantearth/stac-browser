f68fb130dfb25da1a79e1cb82c570e9da1f8b9fe
workmap <tyler.mccracken@critigen.com>
2023-12-21 15:40:25 -0500

    set to GP catalog as default

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    catalogUrl: null,
+    catalogUrl: 'https://sit-stac.geoplatform.info/catalog.json',
     catalogTitle: "STAC Browser",
     allowExternalAccess: true, // Must be true if catalogUrl is not given
     allowedDomains: [],
