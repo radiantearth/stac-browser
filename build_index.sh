9b739f01a1d3d761499c7d7032af1b4c7c83f939
workmap <tyler.mccracken@critigen.com>
2024-04-09 10:29:58 -0400

    add links to header

--- build_index.sh
+++ build_index.sh
@@ -1,11 +1,15 @@
-cp -f ./src/index_template.html ./public/index.html
+cp -fp ./src/index_template.html ./public/index.html
 
 if [ "$STG_ENV" = "sit" ]; then
     endpoint="https://sit.geoplatform.info"
+    support="https://sit-kb.geoplatform.info/"
 elif [ "$STG_ENV" = "stg" ]; then
     endpoint="https://stg.geoplatform.gov"
+    support="https://stg-kb.geoplatform.gov"
 elif [ "$STG_ENV" = "prd" ]; then
     endpoint="https://www.geoplatform.gov"
+    support="https://kb.geoplatform.gov"
 fi
 
-sed -i.bak "s|#PUBLIC_URL#|${endpoint}|g" ./public/index.html
+sed -i '' "s|#PUBLIC_URL#|${endpoint}|g" ./public/index.html
+sed -i '' "s|#SUPPORT_URL#|${support}|g" ./public/index.html
