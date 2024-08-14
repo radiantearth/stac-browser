424c304573da34d76f1cb30b161ff21471ff68f8
Jonathan Chemla <jos.chemla@gmail.com>
2024-08-14 22:11:03 +0200

    Reference new actions in assetActions config

--- assetActions.config.js
+++ assetActions.config.js
@@ -1,9 +1,15 @@
 import CopcViewer from './src/actions/assets/CopcViewer.js';
 import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
 //import Felt from './src/actions/assets/Felt.js';
+import Potree from './src/actions/assets/Potree.js';
+import CesiumSandcastle from './src/actions/assets/CesiumSandcastle.js';
+import Viewer3D from './src/actions/assets/Viewer3D.js';
 
 export default {
   CopcViewer,
   GeoJsonIo,
+  Potree,
+  CesiumSandcastle,
+  Viewer3D,
 //Felt
 };
