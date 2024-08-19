0181e03bb38da60b53e0fec07c5f347b9fef7e36
Jonathan Chemla <jos.chemla@gmail.com>
2024-08-19 12:16:09 +0200

    Add Protomaps reader for pmtiles

--- assetActions.config.js
+++ assetActions.config.js
@@ -2,14 +2,16 @@ import CopcViewer from './src/actions/assets/CopcViewer.js';
 import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
 //import Felt from './src/actions/assets/Felt.js';
 import Potree from './src/actions/assets/Potree.js';
+import Protomaps from './src/actions/assets/Protomaps.js';
 import CesiumSandcastle from './src/actions/assets/CesiumSandcastle.js';
 import Viewer3D from './src/actions/assets/Viewer3D.js';
 
 export default {
+  CesiumSandcastle,
   CopcViewer,
   GeoJsonIo,
   Potree,
-  CesiumSandcastle,
+  Protomaps,
   Viewer3D,
 //Felt
 };
---
+++ src/actions/assets/Protomaps.js
@@ -0,0 +1,27 @@
+import AssetActionPlugin from "../AssetActionPlugin";
+import URI from 'urijs';
+import i18n from "../../i18n";
+
+// obj & ply files are usually with mime-type text/plain 
+const PROTOMAPS_SUPPORTED_TYPES = [
+  'application/vnd.pmtiles',
+];
+
+export default class Protomaps extends AssetActionPlugin {
+
+  get show() {
+    // Can we rather check if .pmtiles substring present in this.asset.href or simply this.component.filename.endsWith('pmtiles')
+    return true || this.component.isBrowserProtocol && PROTOMAPS_SUPPORTED_TYPES.includes(this.asset.type);
+  }
+
+  get uri() {
+    let uri = new URI("https://pmtiles.io/");
+    uri.addQuery("url", this.component.href); // returns the URI instance for chaining
+    return uri;
+  }
+
+  get text() {
+    return i18n.t('actions.openIn', {service: 'Protomaps'});
+  }
+
+}
