5c96656f81945e61ace7a300bb7ad23b4f208ab4
Jonathan Chemla <jos.chemla@gmail.com>
2024-08-28 09:56:36 +0200

    Add links actions for 3d-tiles, protomaps
    Add links actions for OGC 3d-tiles (geofox.ai or cesium sandcastle) and protomaps

--- linkActions.config.js
+++ linkActions.config.js
@@ -1,5 +1,9 @@
-//import Felt from './src/actions/links/Felt.js';
+// import Felt from './src/actions/links/Felt.js';
+import CesiumSandcastle from './src/actions/links/CesiumSandcastle.js';
+import Protomaps from './src/actions/links/Protomaps.js';
 
 export default {
-//Felt
+    // Felt, 
+    CesiumSandcastle, 
+    Protomaps
 };
