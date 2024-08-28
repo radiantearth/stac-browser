c04bde5b8a9a59e63d3a61f90f6729b77b322dde
Jonathan Chemla <jos.chemla@gmail.com>
2024-08-28 10:10:42 +0200

    Rename CesiumSandcastle files and components to OGC3dTiles

--- assetActions.config.js
+++ assetActions.config.js
@@ -4,16 +4,16 @@ import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
 import Geoparquet from './src/actions/assets/Geoparquet.js';
 import Potree from './src/actions/assets/Potree.js';
 import Protomaps from './src/actions/assets/Protomaps.js';
-import CesiumSandcastle from './src/actions/assets/CesiumSandcastle.js';
+import OGC3dTiles from './src/actions/assets/OGC3dTiles.js';
 import Viewer3D from './src/actions/assets/Viewer3D.js';
 
 export default {
-  CesiumSandcastle,
+  //Felt
+  OGC3dTiles,
   CopcViewer,
   GeoJsonIo,
   Geoparquet,
   Potree,
   Protomaps,
   Viewer3D,
-//Felt
 };
--- linkActions.config.js
+++ linkActions.config.js
@@ -1,9 +1,9 @@
 // import Felt from './src/actions/links/Felt.js';
-import CesiumSandcastle from './src/actions/links/CesiumSandcastle.js';
+import OGC3dTiles from './src/actions/links/OGC3dTiles.js';
 import Protomaps from './src/actions/links/Protomaps.js';
 
 export default {
     // Felt, 
-    CesiumSandcastle, 
+    OGC3dTiles, 
     Protomaps
 };
