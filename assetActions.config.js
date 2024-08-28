29866e08f6c1bc7fa4d18fdb631c770c27215419
Jonathan Chemla <jos.chemla@gmail.com>
2024-08-28 15:24:09 +0200

    Remove Geoparquet following m-mohr suggestion

--- assetActions.config.js
+++ assetActions.config.js
@@ -1,7 +1,7 @@
 import CopcViewer from './src/actions/assets/CopcViewer.js';
 import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
 //import Felt from './src/actions/assets/Felt.js';
-import Geoparquet from './src/actions/assets/Geoparquet.js';
+// import Geoparquet from './src/actions/assets/Geoparquet.js';
 import Potree from './src/actions/assets/Potree.js';
 import Protomaps from './src/actions/assets/Protomaps.js';
 import OGC3dTiles from './src/actions/assets/OGC3dTiles.js';
@@ -12,7 +12,7 @@ export default {
   OGC3dTiles,
   CopcViewer,
   GeoJsonIo,
-  Geoparquet,
+  // Geoparquet, // not ready yet
   Potree,
   Protomaps,
   Viewer3D,
