3320c40621bda87f876c0a8b73afcebc431da889
Jonathan Chemla <jos.chemla@gmail.com>
2024-08-29 11:37:45 +0200

    Update potree action
    The `load_potree_project_from_urlparam.html` has been updated to parse more urlparams, simplified the action by passing a JSON-parseable array of one potree dataset url rather than a complete potree project - which would allow for measurements, volumes, cam pos etc.
    Also added a comment to https://potree.org/potree/examples/load_potree_project_from_urlparam.html which should be live once PR is merged by potree maintaining team

--- src/actions/assets/Potree.js
+++ src/actions/assets/Potree.js
@@ -25,50 +25,14 @@ export default class Potree extends AssetActionPlugin {
 
   get uri() {
     // Docs: PR made to original potree repo to avoid relying on iconem own infrastructure
-    // https://github.com/potree/potree/pull/1456
+    // https://github.com/potree/potree/pull/1456 
+    // would be accessible via https://potree.org/potree/examples/load_potree_project_from_urlparam.html
+    // Can also parse pointSize, FOV, opacity, edlEnabled, edlRadius, edlStrength, pointBudget, showBoundingBox, pointSizing, quality, position, target, background via loadSettingsFromURL
     let uri = new URI("https://3d.iconem.com/tools/load_potree_project_from_urlparam.html");
-    uri.addQuery('fit', true);
-    uri.addQuery('c', 'elevation'); // rgba, elevation, intensity etc
-
     const datasetUrl = this.component.href;
-    const potreeProject = {
-        "type": "Potree",
-        "version": 1.7,
-        "pointclouds": [
-            {
-                "url": datasetUrl,
-                "name": "Potree or COPC dataset",
-                "position": [0, 0, 0],
-                "rotation": [0, 0, 0, "XYZ" ],
-                "scale": [1, 1, 1],
-                "activeAttributeName": "rgba"
-            }
-        ],
-        "settings": {
-            "pointBudget": 1000000,
-            "fov": 60,
-            "edlEnabled": true,
-            "edlStrength": 0.4,
-            "background": "gradient",
-            "edlRadius": 1.4,
-            "minNodeSize": 30,
-            "showBoundingBoxes": false
-        },
-        "measurements": [],
-        "volumes": [],
-        "cameraAnimations": [],
-        "profiles": [],
-        "annotations": [],
-        "objects": [],
-        // view can be anything since it is overwritten via url-param fit=true
-        "view": {
-            "position": [ 10, 10, 10 ],
-            "target": [ 0, 0, 0 ]
-        },
-    };
-    // uri.addQuery('potreeProjectJson', JSON.stringify(potreeProject));
-    console.log(potreeProject);
-    uri.addQuery('potreeDatasetUrl', datasetUrl); // potreeDatasetUrl or shortcut r
+    uri.addQuery('fit', 'true');
+    uri.addQuery('c', 'elevation'); // rgba, elevation, intensity etc
+    uri.addQuery('datasetsUrls', `["${datasetUrl}"]`); 
     return uri;
   }
 
@@ -78,8 +42,7 @@ export default class Potree extends AssetActionPlugin {
     uri.addQuery('c', 'rgba');
     uri.addQuery('r', this.component.href);
     // color type can be among rgba, elevation, color, elevation, etc
-    // potree_cloud_js_url = https://potree.org/potree/pointclouds/vol_total/cloud.js
-    // potree_darren_wiens_url = `https://mpc-copc-viewer.netlify.app/?r=${potree_cloud_js_url}&c=rgba`
+    // https://potree.org/potree/examples/copc.html?c=rgba&r=
     return uri;
   }
 
