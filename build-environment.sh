650d4a6be7d3ec331ca1880de8ce7bac21373bfe
Chris MacDermaid <cmacdermaid@xentity.com>
2024-09-09 18:44:00 +0000

    update build

--- build-environment.sh
+++ build-environment.sh
@@ -2,12 +2,15 @@ cp -fp ./src/index_template.html ./public/index.html
 
 if [ "$STG_ENV" = "sit" ]; then
     endpoint="https://sit.geoplatform.info"
+    catalog_endpoint="https://sit-stac.geoplatform.info"
     support="https://sit-kb.geoplatform.info"
 elif [ "$STG_ENV" = "stg" ]; then
     endpoint="https://stg.geoplatform.gov"
+    catalog_endpoint="https://stg-stac.geoplatform.gov"
     support="https://stg-kb.geoplatform.gov"
 elif [ "$STG_ENV" = "prd" ]; then
     endpoint="https://www.geoplatform.gov"
+    catalog_endpoint="https://stac.geoplatform.gov"
     support="https://kb.geoplatform.gov"
 fi
 
@@ -15,4 +18,4 @@ sed -i "s|#PUBLIC_URL#|${endpoint}|g" ./public/index.html
 sed -i "s|#SUPPORT_URL#|${support}|g" ./public/index.html
 
 sed -i "s|#PUBLIC_URL#|${endpoint}|g" ./src/theme/page.scss
-sed -i "s|#PUBLIC_URL#|${endpoint}|g" ./config.js
+sed -i "s|#PUBLIC_URL#|${catalog_endpoint}|g" ./config.js
