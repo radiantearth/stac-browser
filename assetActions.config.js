469d78ef7d7057f543fc2d95da10f8091dad36e3
Jonathan Chemla <jos.chemla@gmail.com>
2025-01-09 17:27:59 +0100

    Remove potree temporarily
    Co-authored-by: Matthias Mohr <matthias@mohr.ws>

--- assetActions.config.js
+++ assetActions.config.js
@@ -5,7 +5,7 @@ import F3D from './src/actions/assets/F3D.js';
 import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
 // import Geofox from './src/actions/assets/Geofox.js';
 // import Geoparquet from './src/actions/assets/Geoparquet.js';
-import Potree from './src/actions/assets/Potree.js';
+// import Potree from './src/actions/assets/Potree.js';
 import Protomaps from './src/actions/assets/Protomaps.js';
 
 export default {
