63991cfc54e9a047255e799dd44838e2ff5017c4
Jonathan Chemla <jos.chemla@gmail.com>
2025-04-15 16:41:22 +0200

    Add ThreePipe viewer for 3d static meshes
    via url threepipe.org/examples/tweakpane-editor?model=URL

--- assetActions.config.js
+++ assetActions.config.js
@@ -2,6 +2,7 @@
 import Cesium from './src/actions/assets/Cesium.js';
 import CopcViewer from './src/actions/assets/CopcViewer.js';
 import F3D from './src/actions/assets/F3D.js';
+import ThreePipe from './src/actions/assets/ThreePipe.js';
 import GeoJsonIo from './src/actions/assets/GeoJsonIo.js';
 // import NASA_AMMOS_3DTiles from './src/actions/assets/NASA_AMMOS_3DTiles.js';
 // import Geoparquet from './src/actions/assets/Geoparquet.js';
@@ -13,6 +14,7 @@ export default {
   Cesium,
   CopcViewer,
   F3D,
+  ThreePipe,
   GeoJsonIo,
   // Geoparquet, // not ready yet
   // Potree,
