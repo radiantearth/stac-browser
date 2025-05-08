3f4dbe7bee4cb8862f3eaf3fb90c383d6d045e84
arasyidi <amri.geodesy@gmail.com>
2025-05-08 23:24:31 +0700

    added duration locale

--- src/locales/id/duration.js
+++ src/locales/id/duration.js
@@ -1,2 +1,10 @@
-import { id as locale } from '@musement/iso-duration';
-export default locale;
+const locale = {
+  years(c) { return "tahun"; },
+  months(c) { return "bulan"; },
+  weeks(c) { return "minggu"; },
+  days(c) { return "hari"; },
+  hours(c) { return "jam"; },
+  minutes(c) { return "menit"; },
+  seconds(c) { return "detik"; },
+  decimal: ",",
+};
