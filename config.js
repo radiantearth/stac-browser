9d69336fe88e0dc3c206efa450902e31f2f4699a
Amri Rasyidi <amri.geodesy@gmail.com>
2025-05-09 01:36:01 +0700

    Add Indonesian locale (#595)
    * finished with first 3
    
    * second part completed
    
    * default files added
    
    * added id to config
    
    * remove comments
    
    Co-authored-by: Matthias Mohr <matthias@mohr.ws>
    
    * deleted stac-field.js
    
    * added duration locale
    
    * minor edit on text
    
    ---------
    
    Co-authored-by: Matthias Mohr <matthias@mohr.ws>

--- config.js
+++ config.js
@@ -24,6 +24,7 @@ module.exports = {
         "ja",
         "pt",
 //      "pt-BR"
+        "id"
     ],
     apiCatalogPriority: null,
     useTileLayerAsFallback: true,
---
+++ src/locales/id/duration.js
@@ -0,0 +1,10 @@
+const locale = {
+  years(c) { return "tahun"; },
+  months(c) { return "bulan"; },
+  weeks(c) { return "minggu"; },
+  days(c) { return "hari"; },
+  hours(c) { return "jam"; },
+  minutes(c) { return "menit"; },
+  seconds(c) { return "detik"; },
+  decimal: ",",
+};
