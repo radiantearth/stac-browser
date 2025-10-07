2231a6cdc81d2f0a7228c4d2754a2bee4da12a2d
Saidy Barry <as.barry@moregeo.it>
2025-10-07 17:47:02 +0200

    fix: VueDatePicker internationalization and date/datetime handling

--- src/components/QueryableInput.vue
+++ src/components/QueryableInput.vue
@@ -17,25 +17,20 @@
           <b-badge variant="dark" class="ml-2">{{ op.label }}</b-badge>
         </b-dropdown-item-button>
       </b-dropdown>
-      
-      <date-picker
-        v-if="queryable.isTemporal"
-        class="value"
-        :value="value.value"
-        @input="updateValue($event)"
-        v-bind="validation"
-      />
 
       <VueDatePicker
         v-if="queryable.isTemporal"
         class="value"
         :model-value="value?.value"
         @update:model-value="updateValue"
-        :locale="datepickerLang"
+        :locale="uiLanguage"
+        :format-locale="datepickerLang"
         :format="queryable.isDateTime ? dateTimeFormat : dateFormat"
         :enable-time-picker="queryable.isDateTime"
+        :time-picker-inline="false"
+        :time-picker-seconds="queryable.isDateTime"
         auto-apply
-        :clearable="true"
+        clearable
       />
 
       <b-form-select
@@ -120,10 +115,7 @@ export default {
   },
   computed: {
     validation() {
-      if (this.queryable.isTemporal) {
-        return {};
-      }
-      else if (this.queryable.isText) {
+      if (this.queryable.isText) {
         return {
           type: 'text',
           minlength: this.schema.minLength,
