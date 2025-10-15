843dbf77fe2aad26674174da03dde573e12734f3
Matthias Mohr <m.mohr@uni-muenster.de>
2025-10-15 14:47:24 +0200

    Fix locales, especially English

--- src/locales/en-US/default.js
+++ src/locales/en-US/default.js
@@ -1,2 +1,8 @@
-import en from '../en/default';
-export default en;
+import Utils from '../../utils';
+export default Utils.mergeDeep(
+  {
+    fields: require('./fields.json')
+  },
+  require('./texts.json'),
+  require('./custom.json')
+);
