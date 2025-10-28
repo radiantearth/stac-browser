0d63fd68c87a6b8c4159c1a448a0c2ece2db93d2
Matthias Mohr <m.mohr@uni-muenster.de>
2025-10-28 11:31:10 +0100

    Fix read-more component

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -1,108 +1,174 @@
 <template>
-  <div class="read-more" :class="{ expanded, 'hide-button': hideButton }">
-    <div ref="content" class="content" :style="{ '--lines': lines }">
+  <div class="text-overflow" :class="{ expanded: expanded, 'no-overflow': inMaxRange }">
+    <div ref="to" class="text-overflow-content">
       <slot />
     </div>
-    
-    <div v-if="!noShadow" class="gradient" />
-    
-    <div v-if="!noButton" @click="toggle" class="toggle">
-      <slot name="more" :open="expanded">
+    <div ref="ht" class="hide-text" />
+    <div @click="toggle" class="button-read-more">
+      <div class="read-more-button">
         <span>{{ expanded ? textLess : text }}</span>
-      </slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted, watch, nextTick } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'ReadMore',
+  name: "ReadMore",
   props: {
-    lines: { type: Number, default: 3 },
-    text: { type: String, default: 'Read more' },
-    textLess: { type: String, default: 'Read less' },
-    noLess: { type: Boolean, default: false },
-    open: { type: Boolean, default: false },
-    noButton: { type: Boolean, default: false },
-    noShadow: { type: Boolean, default: false }
+    lines: {
+      type: Number,
+      default: 3
+    },
+    text: {
+      type: String,
+      default: "Read more"
+    },
+    textLess: {
+      type: String,
+      default: "Read less"
+    }
   },
-  emits: ['update:open'],
-  setup(props, { emit }) {
-    const expanded = ref(props.open);
-    const hideButton = ref(false);
-    const content = ref(null);
-
-    watch(() => props.open, (val) => expanded.value = val);
-
-    const toggle = () => {
-      if (expanded.value && props.noLess) return;
-      expanded.value = !expanded.value;
-      emit('update:open', expanded.value);
+  data() {
+    return {
+      readmore: false,
+      expanded: false,
+      inMaxRange: false
     };
+  },
+  mounted() {
+    const lh = this.getLineHeight(this.$refs.to);
 
-    const checkOverflow = async () => {
-      await nextTick();
-      if (!content.value) return;
-      
-      const style = getComputedStyle(content.value);
-      const lineHeight = parseFloat(style.lineHeight) || parseFloat(style.fontSize) * 1.4;
-      const maxHeight = lineHeight * props.lines;
-      
-      hideButton.value = content.value.scrollHeight <= maxHeight;
-    };
+    if (this.lines) {
+      this.$refs.to.style.setProperty("--nlines", this.lines);
+    }
+    let gLines = 2;
+    if (this.lines > 12) {
+      gLines = 4;
+    } else if (this.lines > 6) {
+      gLines = 3;
+    }
 
-    onMounted(checkOverflow);
+    this.$refs.ht.style.setProperty("--nlines", gLines);
 
-    return { expanded, hideButton, content, toggle };
+    setTimeout(() => {
+      this.readmore = this.$refs.to.offsetHeight < this.$refs.to.scrollHeight;
+
+      const localMaxLines = this.lines + 1 * lh;
+      if (this.$refs.to.scrollHeight <= localMaxLines) {
+        this.inMaxRange = true;
+      }
+
+      this.$refs.to.style.setProperty("--lineHeight", lh + "px");
+      this.$refs.ht.style.setProperty("--lineHeight", lh + "px");
+    });
+  },
+  methods: {
+    toggle() {
+      if (this.expanded) {
+        this.$refs.to.style.removeProperty("max-height");
+        this.expanded = false;
+      } else {
+        this.expanded = true;
+        this.$refs.to.style.setProperty(
+          "max-height",
+          this.$refs.to.scrollHeight + "px"
+        );
+      }
+    },
+    getLineHeight(element) {
+      let temp = document.createElement(element.children[0].nodeName);
+      const cpStyle = getComputedStyle(element.children[0]);
+      temp.setAttribute(
+        "style",
+        "position:absolute;left:-999em;margin:0px;padding:0px;font-family:" +
+          cpStyle.fontFamily +
+          ";font-size:" +
+          cpStyle.fontSize
+      );
+      temp.innerHTML = "test";
+      temp = document.body.appendChild(temp);
+      const ret = temp.clientHeight;
+      temp.parentNode.removeChild(temp);
+      return ret;
+    }
   }
 });
 </script>
 
-<style scoped>
-.read-more {
+<style scoped lang="scss">
+.text-overflow-content {
+  --nlines: 3;
+  --lineHeight: 1.5;
+  max-height: calc(var(--nlines) * var(--lineHeight));
+  overflow: hidden;
+  transition: max-height 0.3s ease;
+}
+
+.text-overflow {
   position: relative;
 }
 
-.content {
-  max-height: calc(var(--lines) * 1.4em);
-  overflow: hidden;
-  transition: max-height 0.3s ease;
-  line-height: 1.4;
+.no-overflow {
+  .text-overflow-content {
+    max-height: 100%;
+    overflow: visible;
+  }
+  .hide-text,
+  .button-read-more {
+    display: none;
+  }
 }
 
-.expanded .content {
-  max-height: none;
-}
-
-.gradient {
-  position: absolute;
-  bottom: 3em;
-  left: 0;
-  right: 0;
-  height: 2em;
-  background: linear-gradient(transparent, white);
-  pointer-events: none;
-}
-
-.expanded .gradient,
-.hide-button .gradient,
-.hide-button .toggle {
-  display: none;
-}
-
-.toggle {
+.read-more-button {
   cursor: pointer;
+  display: block;
+  position: relative;
+  border-top: 1px solid #dbdbdb;
+  height: 0.1em;
+  margin: 0.5em auto 1.5em auto;
+  width: 95%;
   text-align: center;
-  margin-top: 1em;
-  padding: 0.5em;
-  border-top: 1px solid #ddd;
-  color: #666;
-  font-size: 0.9em;
+
+  span {
+    background: #fff;
+    color: #b5b5b5;
+    display: inline-block;
+    font-size: 0.75em;
+    padding: 0.4em 0.8em;
+    transform: translateY(-1.1em);
+    text-align: center;
+  }
 }
 
-.toggle:hover {
-  color: #333;
+.hide-text {
+  --nlines: 6;
+  --lineHeight: 1.5;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0) 0%,
+    rgba(255, 255, 255, 1) 90%,
+    rgba(255, 255, 255, 1) 100%
+  );
+  width: 100%;
+  height: calc(var(--nlines) * var(--lineHeight));
+  position: absolute;
+  //noinspection CssInvalidFunction
+  transform: translateY(calc(var(--nlines) * -1 * var(--lineHeight)));
+}
+
+.hide-text,
+.read-more-button {
+  transition: opacity 0.3s ease, margin 0.3s ease;
+  opacity: 1;
+}
+
+.expanded .hide-text {
+  opacity: 0;
+  margin-top: 0;
+  margin-bottom: 0;
+  pointer-events: none;
 }
 </style>
