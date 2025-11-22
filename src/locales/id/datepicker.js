59211a74ce7af57a6c7d8c61a35c0af59cdab42b
Matthias Mohr <m.mohr@uni-muenster.de>
2025-11-22 17:20:36 +0100

    3 letter abbrev for indonesian

--- src/locales/id/datepicker.js
+++ src/locales/id/datepicker.js
@@ -4,14 +4,4 @@ const dateFormat = 'dd/MM/yyyy';
 const timeFormat = 'HH.mm.ss';
 const dateTimeFormat = `${dateFormat} ${timeFormat}`;
 
-// short weekday tokens used in compact calendar headers
-const dayValues = ['Mg', 'Sn', 'Sl', 'Rb', 'Km', 'Jm', 'Sb'];
-const super_ = locale.localize.day;
-locale.localize.day = (dayIndex, options = {}) => {
-  if (options.width === 'short') {
-    return dayValues[dayIndex];
-  }
-  return super_(dayIndex, options);
-};
-
 export default { dateFormat, timeFormat, dateTimeFormat, locale };
