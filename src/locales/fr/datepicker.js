d24c23bcfee61b96d0c975680444ad12b19db651
Matthias Mohr <m.mohr@uni-muenster.de>
2025-11-28 20:39:23 +0100

    Fix french datepicker locale

--- src/locales/fr/datepicker.js
+++ src/locales/fr/datepicker.js
@@ -1,5 +1,5 @@
 const dateFormat = 'DD/MM/YYYY';
 const timeFormat = 'H:mm:ss';
 const dateTimeFormat = `${dateFormat} ${timeFormat}`;
-const locale = require('vue2-datepicker/locale/fr');require
+const locale = require('vue2-datepicker/locale/fr');
 export default {dateFormat, timeFormat, dateTimeFormat, locale};
