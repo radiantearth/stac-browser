058515ff9fb39b47309744084e1566065bfd59f2
SethDocherty <sddocherty@gmail.com>
2026-02-16 18:37:02 -0500

    revert style changes

--- src/components/ApiCapabilitiesMixin.js
+++ src/components/ApiCapabilitiesMixin.js
@@ -22,38 +22,38 @@ export const TYPES = {
   // OGC / STAC API - Features
   Items: {
     BasicFilters: [
-      "https://api.stacspec.org/v1.*/ogcapi-features",
-      "http://www.opengis.net/spec/ogcapi-features-1/1.*/conf/core",
+      'https://api.stacspec.org/v1.*/ogcapi-features',
+      'http://www.opengis.net/spec/ogcapi-features-1/1.*/conf/core',
     ],
     CollectionIdFilter: false,
     ItemIdFilter: false,
     // It seems some conformance classes use conf (correct) and some req (deprecated?) after the version number
     CqlFilters: [
-      "http://www.opengis.net/spec/ogcapi-features-3/1.*/*/features-filter",
+      'http://www.opengis.net/spec/ogcapi-features-3/1.*/*/features-filter',
     ],
     Sort: [
-      "https://api.stacspec.org/v1.*/ogcapi-features#sort",
-      "http://www.opengis.net/spec/ogcapi-records-1/1.*/conf/sorting",
+      'https://api.stacspec.org/v1.*/ogcapi-features#sort',
+      'http://www.opengis.net/spec/ogcapi-records-1/1.*/conf/sorting',
     ],
-    FreeText: ["https://api.stacspec.org/v1.*/ogcapi-features#free-text"],
+    FreeText: ['https://api.stacspec.org/v1.*/ogcapi-features#free-text'],
   },
   // STAC API - Item Search
   Global: {
-    BasicFilters: ["https://api.stacspec.org/v1.*/item-search"],
+    BasicFilters: ['https://api.stacspec.org/v1.*/item-search'],
     CollectionIdFilter: true,
     ItemIdFilter: true,
-    CqlFilters: ["https://api.stacspec.org/v1.*/item-search#filter"],
-    Sort: ["https://api.stacspec.org/v1.*/item-search#sort"],
-    FreeText: ["https://api.stacspec.org/v1.*/item-search#free-text"],
+    CqlFilters: ['https://api.stacspec.org/v1.*/item-search#filter'],
+    Sort: ['https://api.stacspec.org/v1.*/item-search#sort'],
+    FreeText: ['https://api.stacspec.org/v1.*/item-search#free-text'],
   },
   // OGC / STAC API - Collections
   Collections: {
-    BasicFilters: ["https://api.stacspec.org/v1.*/collection-search"],
+    BasicFilters: ['https://api.stacspec.org/v1.*/collection-search'],
     CollectionIdFilter: false,
     ItemIdFilter: false,
-    CqlFilters: ["https://api.stacspec.org/v1.*/collection-search#filter"],
-    Sort: ["https://api.stacspec.org/v1.*/collection-search#sort"],
-    FreeText: ["https://api.stacspec.org/v1.*/collection-search#free-text"],
+    CqlFilters: ['https://api.stacspec.org/v1.*/collection-search#filter'],
+    Sort: ['https://api.stacspec.org/v1.*/collection-search#sort'],
+    FreeText: ['https://api.stacspec.org/v1.*/collection-search#free-text'],
   },
 };
 
@@ -65,7 +65,7 @@ export default {
     },
   },
   computed: {
-    ...mapGetters(["supportsConformance"]),
+    ...mapGetters(['supportsConformance']),
 
     conformances() {
       return TYPES[this.type];
