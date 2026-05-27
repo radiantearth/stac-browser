d7b9e0466c18d408718c669b10c3d9c75e6b88e6
Lovisa <lovisa.sandgren@rejlers.se>
2026-05-27 09:03:23 +0200

    swedish language

---
+++ src/locales/sv/datepicker.js
@@ -0,0 +1,7 @@
+import { sv as locale } from 'date-fns/locale';
+
+const dateFormat = 'd.M.yyyy';
+const timeFormat = 'H:mm:ss';
+const dateTimeFormat = `${dateFormat} ${timeFormat}`;
+
+export default {dateFormat, timeFormat, dateTimeFormat, locale};
